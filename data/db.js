36a1141f8c5533bb65b68584afc3fe2853b43f28
Madison Austin <36535036+MaddieDeluxe@users.noreply.github.com>
2021-07-06 17:09:01 -0500

    .

--- data/db.js
+++ data/db.js
@@ -9,6 +9,10 @@ const db = {
         'title' : 'Bridgerton Pose Pack',
         'type': itemTypes.POSE,
         'description': 'A mini pose pack',
+        'requirement': {
+            'title': '',
+            'link':''
+        },
         'downloadLink': '/packages/[MaddieDeluxe] Bridgerton Couples Pose Pack.package',
         'images': 4
     },
@@ -16,6 +20,10 @@ const db = {
         'title' : 'Toddler & Adult Telescope Poses',
         'type': itemTypes.POSE,
         'description': '2 Poses designed for use with 1 adult and 1 toddler!',
+        'requirement': {
+            'title': 'Interactions from the past by <b> thepancake1 </b> for telescope.',
+            'link':'https://www.patreon.com/posts/interactions-v5a-51448339?utm_medium=clipboard_copy&utm_source=copy_to_clipboard&utm_campaign=postshare'
+        },
         'downloadLink': '/packages/[MaddieDeluxe] TelescopePosesWithToddler.package',
         'images': 2
     }
--- data/generator.js
+++ data/generator.js
@@ -4,53 +4,37 @@ $(document).ready(function(){
 
 function loadDownloads() {
     for (const [key, value] of Object.entries(db)) {
-        let parentNode = document.createElement("div");
-        parentNode.classList.add('card', 'tile');
+        let holderNode = document.createElement("div");
+        holderNode.classList.add('col-md-3', 'download-holder');
 
+        let parentNode = document.createElement("a");
+        parentNode.href = '#';
+        parentNode.setAttribute('data-toggle', 'modal');
+        parentNode.setAttribute('data-target', '#previewModal');
+        parentNode.setAttribute('data-key', key);
+        parentNode.classList.add('card', 'frosted-glass');
+
+        let thumbnailHolder = document.createElement('div');
+        thumbnailHolder.classList.add('thumbnail-holder', 'p-3');
         let thumbnail = document.createElement('img');
+        thumbnail.classList.add('card-img-top');
         thumbnail.src = '/images/' + key + '/' + '1.png';
-
+        thumbnailHolder.appendChild(thumbnail);
+        
         let body = document.createElement('div');
-        body.classList.add('text-left', 'px-4', 'py-2', 'd-flex', 'flex-column');
-        let title = document.createElement('h2');
+        body.classList.add('card-body', 'px-1');
+
+        let title = document.createElement('h3');
         title.innerText = value.title;
 
-        let description = document.createElement('p');
-        description.innerText = value.description;
-
-        let buttonHolder = document.createElement('div');
-        buttonHolder.classList.add('button-holder');
-
-        
-        let previewButton = document.createElement('a');
-        previewButton.classList.add('btn', 'btn-info');
-        previewButton.innerText = 'Preview';
-        previewButton.setAttribute('data-toggle', 'modal');
-        previewButton.setAttribute('data-target', '#previewModal');
-        previewButton.setAttribute('data-key', key);
-        previewButton.setAttribute('data-title', value.title);
-        previewButton.setAttribute('data-description', value.description);
-        previewButton.setAttribute('data-download', value.downloadLink);
-        previewButton.setAttribute('data-img', value.images);
-
-        buttonHolder.appendChild(previewButton);
-
-        let downloadButton = document.createElement('a');
-        downloadButton.classList.add('btn', 'btn-success', 'ml-1');
-        downloadButton.innerText = 'Download';
-        downloadButton.href = value.downloadLink;
-        buttonHolder.appendChild(downloadButton);
-
         body.appendChild(title);
-        body.appendChild(description);
-        body.appendChild(buttonHolder);
 
-
-        parentNode.appendChild(thumbnail);
+        parentNode.appendChild(thumbnailHolder);
         parentNode.appendChild(body);
 
+        holderNode.appendChild(parentNode);
         var downloads = document.getElementById('downloads');
-        downloads.appendChild(parentNode);
+        downloads.appendChild(holderNode);
     }
 }
 
@@ -58,18 +42,30 @@ function loadDownloads() {
 $('#previewModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var key = button.data('key') // Extract info from data-* attributes
-    var title = button.data('title') // Extract info from data-* attributes
-    var description = button.data('description') // Extract info from data-* attributes
-    var download = button.data('download') // Extract info from data-* attributes
-    var img = button.data('img') // Extract info from data-* attributes
+
+    var object = db[key];
 
     var modal = $(this)
-    modal.find('.modal-title').text(title)
-    modal.find('.description').text(description)
-    modal.find('.download').attr('href', download);
+    modal.find('.modal-title').text(object.title);
+    modal.find('.description').text(object.description);
+    modal.find('.download').attr('href', object.downloadLink);
 
+    if (object.requirement.title != '') {
+        let alert = document.createElement('a');
+        alert.target = '_blank';
+        alert.href = object.requirement.link;
+        alert.innerHTML = '<b>Required: </b>' + object.requirement.title;
+        alert.classList.add('alert', 'alert-warning', 'd-block', 'text-decoration-none', 'shadow-sm', 'border-0');
+        
+        modal.find('.requirement').html(alert);
+    } else {
+        modal.find('.requirement').html('');
+    }
+
+
+    modal.find('.download').attr('href', object.downloadLink);
     var carouselItems = document.createElement('div');
-    for (let i = 1; i <= img; i++) {
+    for (let i = 1; i <= object.images; i++) {
         let carouselItem = document.createElement('div');
 
         if(i == 1) {
